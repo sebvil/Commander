43f2fbf102c4112fcc11a96e63bc191009fa2f9e
sebvil <sebasvime@gmail.com>
2018-08-14 14:15:37 -0500

    Clears existing containeers and links shock server to port 7445

--- new_shock.sh
+++ new_shock.sh
@@ -3,11 +3,14 @@
 #creates network if it does not exist
 sudo docker network create mongo-shock
 
+sudo docker rm -f mongo
+sudo docker rm -f shock-server
+
 #creates mongo container if it does not exist
 
 sudo docker run -d --name mongo --network mongo-shock -v mongo:/data/db mongo:3.6
 
 #create shock container
 
-sudo docker run -d -ti --name shock-server --network mongo-shock -v shock:/usr/local/shock mgrast/shock
+sudo docker run -d -ti --name shock-server --network mongo-shock --rm -p 7445:7445 -v shock:/usr/local/shock mgrast/shock
 
